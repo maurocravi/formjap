d54c2467e634f94a85ce318ca73566d8d11a90a4
maurocravi <maurocravi@gmail.com>
2023-09-16 18:51:30 -0300

    subiendo los archivos

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      noto: ["Noto Sans TC"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
